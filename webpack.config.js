a90644c61640c4040752ab4062e7c9e6a400ce5b
Gusenov Marat <gusenovmaratr@gmail.com>
2025-11-17 22:44:48 +0300

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,162 +1,243 @@
 const path = require("path");
+const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const fs = require('fs');
 
-const isDev = process.env.NODE_ENV === "development"
-const isProd = !isDev
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
 
-const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
-
-if (mode = isProd) {
-  mode = "production"
-} else if (mode = isDev) {
-  mode = "development"
-}
-console.log(mode + " mode")
-
-let entryName = {};
-
-function entryPoints(page) {
-  entryName[page] = `./pages/${page}`;
-}
+const filename = (ext, folder = '') => isDev 
+  ? `${folder}[name].${ext}` 
+  : `${folder}[name].[contenthash:5].${ext}`;
 
 const PATHS = {
   src: path.join(__dirname, "./src"),
   dist: path.join(__dirname, "./dist"),
   assets: 'assets/'
-}
+};
 
-const PAGES_DIR = `${PATHS.src}/pages/.`
-const PAGES = fs
-  .readdirSync(PAGES_DIR);
+const PAGES_DIR = `${PATHS.src}/pages`;
+const PAGES = fs.existsSync(PAGES_DIR) 
+  ? fs.readdirSync(PAGES_DIR).filter(page => {
+      const pagePath = path.join(PAGES_DIR, page);
+      return fs.statSync(pagePath).isDirectory();
+    })
+  : [];
+
+const entryPoints = {};
+if (PAGES.length > 0) {
+  PAGES.forEach(page => {
+    entryPoints[page] = `./pages/${page}/${page}.ts`;
+  });
+} else {
+  entryPoints.index = {
+    dependOn: 'slider',
+    import: ['./demo/index.ts', './demo/index.scss'],
+  };
+  entryPoints.slider = ['./slider/slider.ts', './slider/styles/slider.scss'];
+}
 
 const plugins = () => {
   const base = [
+    new CleanWebpackPlugin(),
+    
     new CopyPlugin({
       patterns: [
         {
           from: `${PATHS.src}/${PATHS.assets}img`,
-          to: `${PATHS.dist}/${PATHS.assets}img`,
+          to: `${PATHS.dist}/img`,
         },
         {
           from: `${PATHS.src}/${PATHS.assets}fonts`,
-          to: `${PATHS.dist}/${PATHS.assets}fonts`,
+          to: `${PATHS.dist}/fonts`,
         },
       ],
+      options: {
+        concurrency: 100,
+      },
     }),
+
     new MiniCssExtractPlugin({
-      filename: filename("css"),
+      filename: filename("css", "css/"),
     }),
-    ...PAGES.map((page) => {
-      entryPoints(page);
-      return new HTMLWebpackPlugin({
+  ];
+
+  if (PAGES.length > 0) {
+    PAGES.forEach(page => {
+      base.push(new HTMLWebpackPlugin({
         template: `./pages/${page}/${page}.pug`,
         filename: `./${page}.html`,
         chunks: [`${page}`],
-      });
-    }),
-  ];
+      }));
+    });
+  } else {
+    base.push(new HTMLWebpackPlugin({
+      template: `./demo/page/page.pug`,
+      filename: `index.html`,
+      chunks: ['index', 'slider'],
+    }));
+  }
+
+  if (isDev) {
+    base.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'windows.jQuery': 'jquery',
+      })
+    );
+  }
+
   return base;
 };
 
+const minimizer = () => {
+  if (isProd) {
+    return [new TerserPlugin(), new CssMinimizerPlugin()];
+  }
+  return [];
+};
+
 module.exports = {
-  context: PATHS.src,
-  mode: mode,
-  entry: entryName,
-  output: {
-    filename: filename("js"),
-    path: PATHS.dist,
-    assetModuleFilename: "assets/[hash][ext][query]",
-		clean: true,
+  stats: {
+    errorDetails: true,
+    children: true,
   },
-	performance: {
-		maxEntrypointSize: 512000,
-		maxAssetSize: 512000
-	},
-	devtool: isDev ? 'inline-source-map' : false,
-  devServer: isDev ? {
-		port: 5000,
-		open: true
-  } : undefined,
+
+  context: PATHS.src,
+  mode: isProd ? "production" : "development",
+  target: isDev ? 'web' : 'browserslist',
+  
+  entry: entryPoints,
+
+  output: {
+    filename: filename("js", "js/"),
+    path: PATHS.dist,
+    publicPath: "",
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true,
+  },
+
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+
+  devtool: isDev ? 'inline-source-map' : false,
+  
+  devServer: {
+    static: {
+      directory: PATHS.dist,
+    },
+    port: isDev ? 5000 : 8008,
+    hot: true,
+    open: true
+  },
+
   optimization: {
+    minimize: isProd,
+    minimizer: minimizer(),
     splitChunks: {
       chunks: "all"
     },
   },
+
   resolve: {
-		extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@fonts': path.resolve(__dirname, 'src/assets/fonts'),
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
   plugins: plugins(),
+
   module: {
-    rules:
-      [
-        {
-          test: /\.html$/i,
-          loader: "html-loader",
-        },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            (isDev) ? "style-loader" : MiniCssExtractPlugin.loader,
-            "css-loader",
-            {
-              loader: "postcss-loader",
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    [
-                      "postcss-preset-env",
-                      {
-                        // Options
-                      },
-                    ],
-                  ],
-                },
-              },
-            },
-            "sass-loader",
-          ],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/,
-          type: "asset/resource",
-        },
-        {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.(js|jsx|tsx|ts)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: ['babel-loader', 'ts-loader'],
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          isDev ? "style-loader" : {
+            loader: MiniCssExtractPlugin.loader,
             options: {
-              presets: ["@babel/preset-env"],
-              plugins: ['@babel/plugin-proposal-class-properties'],
+              publicPath: "../",
             },
           },
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: 'img/[name][contenthash][ext]'
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][contenthash:5][ext]'
+        }
+      },
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-typescript",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              '@babel/plugin-transform-class-properties'
+            ],
+          },
         },
-        {
-					test: /\.pug$/,
-					use: [
-						{
-							loader: 'pug-loader',
-							options: {
-								pretty: true,
-								self: true,
-								doctype: 'html',
-								compileDebug: true,
-							}
-						}
-					]
-				}
-      ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'pug-loader',
+            options: {
+              pretty: true,
+              self: true,
+              doctype: 'html',
+              compileDebug: true,
+            }
+          }
+        ]
+      }
+    ],
   },
-}
+};
